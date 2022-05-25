ea59f3577e9b7085b5f4a59765898784fb2ab370
Julian <jwork365@users.noreply.github.com>
2022-05-25 13:17:25 -0700

    init

---
+++ precache-manifest.5245956a569bbc3fc0852e5682e15e71.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5918facd830152074fc2",
+    "url": "/css/about.f032bb27.css"
+  },
+  {
+    "revision": "5837503e0c8309937f8f",
+    "url": "/css/app.316c0cbb.css"
+  },
+  {
+    "revision": "f3911b3804eb8be072dd",
+    "url": "/css/chunk-17edbcc2.000306f6.css"
+  },
+  {
+    "revision": "8e84711d3babf0bf58c4",
+    "url": "/css/chunk-1feb7669.bd72455a.css"
+  },
+  {
+    "revision": "a5fc90a168a6c801e25a",
+    "url": "/css/chunk-4b4c1790.ebe3402c.css"
+  },
+  {
+    "revision": "21293e85e4916aa7aeb7",
+    "url": "/css/chunk-6130c54c.4a368da4.css"
+  },
+  {
+    "revision": "27cf829dde5f46c823c5",
+    "url": "/css/chunk-7904c031.70e39d78.css"
+  },
+  {
+    "revision": "c6782bc59a3b7a782c08",
+    "url": "/css/chunk-bb07c0bc.45330e45.css"
+  },
+  {
+    "revision": "dab778c52d77dd04c4a3",
+    "url": "/css/chunk-dd08163e.26c6674d.css"
+  },
+  {
+    "revision": "d90df0bff53e6aec8fe7",
+    "url": "/css/chunk-vendors.521450e0.css"
+  },
+  {
+    "revision": "11787a8ff3380424c658db7792f0c29f",
+    "url": "/img/about-bg.svg"
+  },
+  {
+    "revision": "23df53fe1a3f3fb68877170afaaa0321",
+    "url": "/img/avatar.mp4"
+  },
+  {
+    "revision": "ac67bbc67aebb087a61c475b9ac09dcf",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "8407002080e519ffc0017bb78bdc281c",
+    "url": "/img/up-outline.svg"
+  },
+  {
+    "revision": "41810df00c0d4602ec47707a3e73c629",
+    "url": "/img/works/086/cover.jpg"
+  },
+  {
+    "revision": "18e3e977ea590d88be4214bf8dcb7b9f",
+    "url": "/img/works/087/cover-1.jpg"
+  },
+  {
+    "revision": "7e444cc65496e8ab8b7767d2af44acb3",
+    "url": "/img/works/087/cover.jpg"
+  },
+  {
+    "revision": "b4763e7a4497681236b1f934b40c103a",
+    "url": "/img/works/087/dev-1.jpg"
+  },
+  {
+    "revision": "9fd21299b39be4a07132e12a54211d46",
+    "url": "/img/works/087/ui-1.jpg"
+  },
+  {
+    "revision": "d77ef16a663abe46b7dd3d9f768e448e",
+    "url": "/img/works/087/ui-2.jpg"
+  },
+  {
+    "revision": "8a6e924ef7d04ab7c76caed5024f4dec",
+    "url": "/img/works/088/cover.jpg"
+  },
+  {
+    "revision": "ca145ec2fe1a48c8d79cf67ed8608be9",
+    "url": "/img/works/088/demo.mp4"
+  },
+  {
+    "revision": "7fbe58215436e28e24099e1904dd2a22",
+    "url": "/img/works/088/ui-0.jpg"
+  },
+  {
+    "revision": "ce2a28840b3065b462b568eec542d2f3",
+    "url": "/img/works/088/ui-1.jpg"
+  },
+  {
+    "revision": "d087c743430bdd29b7a977bd73268458",
+    "url": "/img/works/088/ui-2.jpg"
+  },
+  {
+    "revision": "6d33ac2f0093ecf3008ef8f28f56f799",
+    "url": "/img/works/088/ui-3.jpg"
+  },
+  {
+    "revision": "e84378dba81a05d9213fdfcb1cf35ad2",
+    "url": "/img/works/088/ux-0.jpg"
+  },
+  {
+    "revision": "79ca8a236347e2457e359481d1bf39ca",
+    "url": "/img/works/089/cover-1.png"
+  },
+  {
+    "revision": "1501cc505b79469742408c57427ef8e2",
+    "url": "/img/works/089/cover.jpg"
+  },
+  {
+    "revision": "00ed2db96bc37e7fa8d62b6136588771",
+    "url": "/img/works/090/advantages-design.jpg"
+  },
+  {
+    "revision": "363ad142c4549eb15c40a7373920b495",
+    "url": "/img/works/090/advantages-dev.jpg"
+  },
+  {
+    "revision": "8ba5a97e64a2a203eb7ba0309757de03",
+    "url": "/img/works/090/cover-2.jpg"
+  },
+  {
+    "revision": "592effe43bd2e65cae37a6b78c98f41f",
+    "url": "/img/works/090/cover.jpg"
+  },
+  {
+    "revision": "9e0a0e7cf0e7c95cd5618a6118645c6c",
+    "url": "/img/works/090/ui-1.jpg"
+  },
+  {
+    "revision": "0250d8d5b9117f04b65baf67bbde3b35",
+    "url": "/img/works/090/ui-2.jpg"
+  },
+  {
+    "revision": "2ec94adc394db3ff7cbd98f20f28ffb3",
+    "url": "/img/works/090/ui-3.jpg"
+  },
+  {
+    "revision": "bb38d2e5a4e7e04e6baf9e315a4d1431",
+    "url": "/img/works/090/ui-4.jpg"
+  },
+  {
+    "revision": "c37510783e5ad961d7cbebfe0fdf0f70",
+    "url": "/img/works/091/affinity-diagram.jpg"
+  },
+  {
+    "revision": "f61f68d603072c6531328a162ca1ed27",
+    "url": "/img/works/091/cover-2.jpg"
+  },
+  {
+    "revision": "a31e4d8417f261a43b9304bc0f888cda",
+    "url": "/img/works/091/cover.jpg"
+  },
+  {
+    "revision": "c39b579ecb3ab00dc64c4c10e6a0e1ea",
+    "url": "/img/works/091/information-architecture.jpg"
+  },
+  {
+    "revision": "64cef41325d87ca1e7504ac08a3b9fb5",
+    "url": "/img/works/091/keywords.jpg"
+  },
+  {
+    "revision": "7487dd8f3ea431b212d0b0f6270b8639",
+    "url": "/img/works/091/personas.jpg"
+  },
+  {
+    "revision": "5c05afc3e7b1e814bfe329dff961fc66",
+    "url": "/img/works/091/thinking-process.jpg"
+  },
+  {
+    "revision": "7e4f1a26595b626c9eeb125caf54d939",
+    "url": "/img/works/091/ui-0.jpg"
+  },
+  {
+    "revision": "07050280e83b0f1e7acb0d6a256a2268",
+    "url": "/img/works/091/ui-1.jpg"
+  },
+  {
+    "revision": "cc2701afa82090e26dc38264096db288",
+    "url": "/img/works/091/ui-2.jpg"
+  },
+  {
+    "revision": "96c4ce7696682e437106acedb4359fd4",
+    "url": "/img/works/091/ui-3.jpg"
+  },
+  {
+    "revision": "3c3881ce17453fec597f5646538fa61e",
+    "url": "/img/works/091/ui-4.jpg"
+  },
+  {
+    "revision": "42ec34c1264b720998ea12086b154427",
+    "url": "/img/works/091/ux-0.jpg"
+  },
+  {
+    "revision": "cfec63047374cc16bc5aab7482083a86",
+    "url": "/img/works/091/ux-1.jpg"
+  },
+  {
+    "revision": "572b9e9d81b2952ee036934989105c01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5918facd830152074fc2",
+    "url": "/js/about.e3857f0c.js"
+  },
+  {
+    "revision": "5837503e0c8309937f8f",
+    "url": "/js/app.56c00a8f.js"
+  },
+  {
+    "revision": "f3911b3804eb8be072dd",
+    "url": "/js/chunk-17edbcc2.0d77ac62.js"
+  },
+  {
+    "revision": "8e84711d3babf0bf58c4",
+    "url": "/js/chunk-1feb7669.ce6c6d80.js"
+  },
+  {
+    "revision": "a5fc90a168a6c801e25a",
+    "url": "/js/chunk-4b4c1790.497f6527.js"
+  },
+  {
+    "revision": "21293e85e4916aa7aeb7",
+    "url": "/js/chunk-6130c54c.b333973b.js"
+  },
+  {
+    "revision": "27cf829dde5f46c823c5",
+    "url": "/js/chunk-7904c031.d7f8abea.js"
+  },
+  {
+    "revision": "c6782bc59a3b7a782c08",
+    "url": "/js/chunk-bb07c0bc.e100dce1.js"
+  },
+  {
+    "revision": "dab778c52d77dd04c4a3",
+    "url": "/js/chunk-dd08163e.534da839.js"
+  },
+  {
+    "revision": "d90df0bff53e6aec8fe7",
+    "url": "/js/chunk-vendors.9e763985.js"
+  },
+  {
+    "revision": "2423178146ebd0bd75c332ee47a0855e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
