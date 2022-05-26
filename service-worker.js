e7fb39e6eaefdc412580e2d55c2321edc6d65dbc
Julian <jwork365@users.noreply.github.com>
2022-05-26 15:57:41 -0700

    revise text

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3110b13d1d4c69476756851f59114a69.js"
+  "/precache-manifest.153ef8fd6d10a10a989f656f71789fee.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "juliandesign"});
